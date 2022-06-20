689b77024f3c59b66f346d3097848db0524b6f94
Levi <otterbach.levi@gmail.com>
2022-06-20 17:50:49 +0200

    add unversioned files

---
+++ moorhuhn-service/src/main/java/com/moorhuhnservice/moorhuhnservice/MoorhuhnServiceApplication.java
@@ -0,0 +1,13 @@
+package com.moorhuhnservice.moorhuhnservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoorhuhnServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoorhuhnServiceApplication.class, args);
+	}
+
+}
---
+++ moorhuhn-service/src/test/java/com/moorhuhnservice/moorhuhnservice/MoorhuhnServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moorhuhnservice.moorhuhnservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoorhuhnServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
