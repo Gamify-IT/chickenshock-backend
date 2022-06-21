8b3ed540c16298c8ff17147e87a17469fffd00f9
LeviUniAcc <102458061+LeviUniAcc@users.noreply.github.com>
2022-06-21 17:38:02 +0200

    Update moorhuhn-service/src/main/java/com/moorhuhnservice/moorhuhnservice/BaseClasses/Question.java
    Co-authored-by: delvh <dev.lh@web.de>

--- moorhuhn-service/src/main/java/com/moorhuhnservice/moorhuhnservice/BaseClasses/Question.java
+++ moorhuhn-service/src/main/java/com/moorhuhnservice/moorhuhnservice/BaseClasses/Question.java
@@ -8,7 +8,7 @@ public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
-    private Long id;
+    private long id;
     @Column(nullable = false)
     private String level;
     @Column(nullable = false)
