29c7cc49de96523e0cc0414260e449d8963be68e
Levi <otterbach.levi@gmail.com>
2022-06-21 16:46:31 +0200

    let all requests through

--- moorhuhn-service/src/main/java/com/moorhuhnservice/moorhuhnservice/MoorhuhnServiceApplication.java
+++ moorhuhn-service/src/main/java/com/moorhuhnservice/moorhuhnservice/MoorhuhnServiceApplication.java
@@ -2,6 +2,9 @@ package com.moorhuhnservice.moorhuhnservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MoorhuhnServiceApplication {
@@ -9,5 +12,18 @@ public class MoorhuhnServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MoorhuhnServiceApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				// allow CORS requests for all resources and HTTP methods from the frontend origin
+				registry.addMapping("/**")
+						.allowedMethods("OPTIONS", "HEAD", "GET", "PUT", "POST", "DELETE");
+			}
+		};
+	}
+
 
 }
