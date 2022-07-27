037ce28341a1f2c7d9d2abaa1394172025a4d4cb
Levi <otterbach.levi@gmail.com>
2022-07-27 16:12:00 +0200

    change path remove imports

--- src/main/java/de/unistuttgart/moorhuhnbackend/controller/GameResultController.java
+++ src/main/java/de/unistuttgart/moorhuhnbackend/controller/GameResultController.java
@@ -1,19 +1,15 @@
 package de.unistuttgart.moorhuhnbackend.controller;
 
 import de.unistuttgart.moorhuhnbackend.data.GameResultDTO;
-import de.unistuttgart.moorhuhnbackend.data.QuestionDTO;
-import de.unistuttgart.moorhuhnbackend.repositories.GameResultRepository;
-import de.unistuttgart.moorhuhnbackend.service.ConfigService;
 import de.unistuttgart.moorhuhnbackend.service.GameResultService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
 
 @RestController
-@RequestMapping("api/v1/minigames/moorhuhn/results")
+@RequestMapping("/results")
 @Slf4j
 public class GameResultController {
 
