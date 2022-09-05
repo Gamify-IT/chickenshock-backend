d7d71e7589df7112cf3c71e56bbc63124a6c4e05
madebyTimo <t.schnaible@madebytimo.de>
2022-09-05 10:16:26 +0200

    add Token expired handling (Gamify-IT/issues#251)

--- src/main/java/de/unistuttgart/chickenshockbackend/service/JWTValidatorService.java
+++ src/main/java/de/unistuttgart/chickenshockbackend/service/JWTValidatorService.java
@@ -5,6 +5,7 @@ import com.auth0.jwk.JwkProvider;
 import com.auth0.jwk.UrlJwkProvider;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
 import lombok.extern.slf4j.Slf4j;
@@ -78,7 +79,7 @@ public class JWTValidatorService {
             log.info("Verified User " + jwt.getClaim("preferred_username"));
             return jwt;
 
-        } catch (JwkException e) {
+        }catch (TokenExpiredException | JwkException e) {
             ResponseStatusException exception = new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Token is invalid!");
             log.warn("Access denied {}", exception.getMessage());
             throw exception;
