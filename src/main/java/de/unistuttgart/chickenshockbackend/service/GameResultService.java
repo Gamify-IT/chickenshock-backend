6ebdb08993a91672bbae03d77272f6ac823202c1
Levi <otterbach.levi@gmail.com>
2022-09-13 16:06:15 +0200

    add reciliance like finitequiz

--- src/main/java/de/unistuttgart/chickenshockbackend/service/GameResultService.java
+++ src/main/java/de/unistuttgart/chickenshockbackend/service/GameResultService.java
@@ -6,10 +6,14 @@ import de.unistuttgart.chickenshockbackend.data.mapper.RoundResultMapper;
 import de.unistuttgart.chickenshockbackend.repositories.GameResultRepository;
 import de.unistuttgart.chickenshockbackend.repositories.QuestionRepository;
 import java.util.List;
+
+import feign.FeignException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @Slf4j
@@ -37,39 +41,47 @@ public class GameResultService {
    * @param gameResultDTO extern gameResultDTO
    */
   public void saveGameResult(final GameResultDTO gameResultDTO) {
-    final List<RoundResult> correctQuestions = roundResultMapper.roundResultDTOsToRoundResults(
-      gameResultDTO.getCorrectAnsweredQuestions()
-    );
-    final List<RoundResult> wrongQuestions = roundResultMapper.roundResultDTOsToRoundResults(
-      gameResultDTO.getWrongAnsweredQuestions()
-    );
-    //String playerId = authorizationService.getPlayerId(token);TODO: after login is implemented
-    final GameResult result = new GameResult(
-      gameResultDTO.getQuestionCount(),
-      gameResultDTO.getTimeLimit(),
-      gameResultDTO.getFinishedInSeconds(),
-      gameResultDTO.getCorrectKillsCount(),
-      gameResultDTO.getWrongKillsCount(),
-      gameResultDTO.getKillsCount(),
-      gameResultDTO.getShotCount(),
-      gameResultDTO.getPoints(),
-      correctQuestions,
-      wrongQuestions,
-      gameResultDTO.getConfigurationAsUUID(),
-      "playerId"
-    );
-    gameResultRepository.save(result);
-
-    final int resultScore = calculateResultScore(result.getCorrectKillsCount(), result.getQuestionCount());
-    System.out.println(result.getTimeLimit());
-
+    final int resultScore = calculateResultScore(gameResultDTO.getCorrectKillsCount(), gameResultDTO.getQuestionCount());
     final OverworldResultDTO resultDTO = new OverworldResultDTO(
-      "CHICKENSHOCK",
-      gameResultDTO.getConfigurationAsUUID(),
-      resultScore,
-      "1"
+            "CHICKENSHOCK",
+            gameResultDTO.getConfigurationAsUUID(),
+            resultScore,
+            "1"
     );
-    resultClient.submit(resultDTO);
+    try {
+      resultClient.submit(resultDTO);
+      final List<RoundResult> correctQuestions = roundResultMapper.roundResultDTOsToRoundResults(
+              gameResultDTO.getCorrectAnsweredQuestions()
+      );
+      final List<RoundResult> wrongQuestions = roundResultMapper.roundResultDTOsToRoundResults(
+              gameResultDTO.getWrongAnsweredQuestions()
+      );
+      final GameResult result = new GameResult(
+              gameResultDTO.getQuestionCount(),
+              gameResultDTO.getTimeLimit(),
+              gameResultDTO.getFinishedInSeconds(),
+              gameResultDTO.getCorrectKillsCount(),
+              gameResultDTO.getWrongKillsCount(),
+              gameResultDTO.getKillsCount(),
+              gameResultDTO.getShotCount(),
+              gameResultDTO.getPoints(),
+              correctQuestions,
+              wrongQuestions,
+              gameResultDTO.getConfigurationAsUUID(),
+              "playerId"
+      );
+      gameResultRepository.save(result);
+    } catch (FeignException.BadGateway badGateway) {
+      String warning = "The Overworld backend is currently not available. The result was NOT saved. Please try again later";
+      log.warn(warning + badGateway);
+      throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE,
+              warning);
+    } catch (FeignException.NotFound notFound) {
+      String warning = "The result could not be saved. Unknown User";
+      log.warn(warning + notFound);
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+              warning);
+    }
   }
 
   private int calculateResultScore(final int correctAnswers, final int numberOfQuestions) {
