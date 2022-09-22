7ccfad1e8aff7487c0b4243f7ee95cc41a15e5c7
Levi <otterbach.levi@gmail.com>
2022-09-22 12:54:48 +0200

    fix readme

--- src/main/java/de/unistuttgart/chickenshockbackend/data/OverworldResultDTO.java
+++ src/main/java/de/unistuttgart/chickenshockbackend/data/OverworldResultDTO.java
@@ -1,17 +1,18 @@
 package de.unistuttgart.chickenshockbackend.data;
 
 import de.unistuttgart.chickenshockbackend.Constants;
-import java.util.UUID;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
 
 /**
  * The OverworldResultDTO.class contains all the info that is sent to the Overworld-backend
@@ -29,8 +30,8 @@ public class OverworldResultDTO {
     @NotNull(message = "configurationId cannot be null")
     UUID configurationId;
 
-    @Min(Constants.MIN_SCORE)
-    @Max(Constants.MAX_SCORE)
+    @Min(value = Constants.MIN_SCORE, message = "Score cannot be less than " + Constants.MIN_SCORE)
+    @Max(value = Constants.MAX_SCORE, message = "Score cannot be higher than " + Constants.MAX_SCORE)
     long score;
 
     @NotNull(message = "user cannot be null")
