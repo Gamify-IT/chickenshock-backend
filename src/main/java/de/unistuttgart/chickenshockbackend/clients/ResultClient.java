267e2ecaac77347c40d866ae725883f6f3e54b45
Max <39833217+maexled@users.noreply.github.com>
2022-09-28 17:27:01 +0200

    Pass access token to overworld backend on submit (#35)
    Part of Gamify-IT/issues#286
    
    Co-authored-by: LeviUniAcc <102458061+LeviUniAcc@users.noreply.github.com>

--- src/main/java/de/unistuttgart/chickenshockbackend/clients/ResultClient.java
+++ src/main/java/de/unistuttgart/chickenshockbackend/clients/ResultClient.java
@@ -2,6 +2,7 @@ package de.unistuttgart.chickenshockbackend.clients;
 
 import de.unistuttgart.chickenshockbackend.data.OverworldResultDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
 
 /**
@@ -12,8 +13,9 @@ public interface ResultClient {
     /**
      * Submits the resultDTO to the Overworld-Backend
      *
+     * @param accessToken the users access token
      * @param resultDTO the player submitted result, trimmed down to the data needed for the overworld
      */
     @PostMapping("/submit-game-pass")
-    void submit(OverworldResultDTO resultDTO);
+    void submit(@CookieValue("access_token") final String accessToken, final OverworldResultDTO resultDTO);
 }
